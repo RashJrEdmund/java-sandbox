b107ed8568ba4f9f3f464b721033da27a60b8cc0
rash <orashusedmund@gmail.com>
2024-03-29 00:31:48 +0100

    added intellij project

---
+++ intelli J/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        // VARIABLES
+
+        // 1 STRINGS;
+        String name = "Rash";
+
+        System.out.println("Hello " + name);
+
+        // 2 NUMBERS
+        // 2.1 Integers
+        int age = 15;
+        int maxInteger = Integer.MAX_VALUE;
+        int minInteger = Integer.MIN_VALUE;
+
+        System.out.println("The maximum integer value is = " + maxInteger);
+        System.out.println("The minimum byte value is = " + minInteger);
+
+        System.out.println("Your age is " + age);
+
+        // 2.2 Bytes
+        byte maxByteValue = Byte.MAX_VALUE;
+        byte minByteValue = Byte.MIN_VALUE;
+
+        System.out.println("The maximum byte value is = " + maxByteValue);
+        System.out.println("The minimum byte value is = " + minByteValue);
+
+        // 2.3 Long
+        long maxLongValue = Long.MAX_VALUE;
+        long minLongValue = Long.MIN_VALUE;
+
+        System.out.println("The maximum long value is = " + maxLongValue);
+        System.out.println("The minimum long value is = " + minLongValue);
+    }
+}
